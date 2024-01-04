0d65f6a9626cc089ed8ce91d08b68e50819648e5
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-04 23:31:44 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ var arrr = [1,2,3,['mhr','you ','rezwan',{In_heartOf_Mohabbat:'Maria'}],'love ']
 document.write(arrr[4])
 document.write(arrr[3][1]);
 document.write(arrr[3][3].In_heartOf_Mohabbat)
+
+var obj = {fuit:'Mango',fish:'All',Mohabbat:['Maria','Jim']}
+document.write(obj)
+document.write(Mohabbat[0])

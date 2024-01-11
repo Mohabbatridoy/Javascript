7ff6370b4d758d4038026267211947cf79302621
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-11 22:40:14 +0600

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+strign = prompt("Please Enter the string: ")
+
+
+
+if (strign.length%2===0){
+ console.log("the lenght of the string is even")
+}
+else{
+    console.log("the length of the string is odd")
+}

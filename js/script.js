c629617c4df5e220a12d569a05bf1b085bc3ba47
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-12 09:12:58 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 var i = 1;
-while(i<=5){
-    console.log("this is "+i)
+var mul = 1;
+
+while(i<=10){
+    mul = mul*i;
     i++;
+    console.log("print")
 }
-console.log("this is out of the loop")
+console.log("the mul is : "+mul)
+console.log("End of while loop")
+console.log("\n")
+console.log("start of for loop")
+// now lets start for loop: 
+
+var i;
+for(i=0; i<=10; i++){
+    console.log(i)
+}
+
+var j
+for (j=5; j>=1; j--){
+    console.log(j)
+}

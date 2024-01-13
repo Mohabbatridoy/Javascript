af97fe89df181f09245b538438fb35d7425f4917
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-13 19:10:12 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-var inp = prompt("Please Enter input: ")
-
-inp = parseInt(inp)
+var inp = parseInt(prompt("Please Enter input: "));
 sum = 0;
+series = "";
 
-for (var i=1; i<inp; i++){
+
+for (var i=1; i<=inp; i++){
     sum = sum + i**2;
-    console.log(`${i} + ${i**2} = ${sum}`)
+    series += (i**2).toString();
+    if (i==inp) {continue;}
+    series +=" + ";
+   
+
 }
+
+if (i>inp){
+    series += " + ..... + n"
+}
+console.log(`${series} = ${sum}`)
+
 

5e5edde72e135e77bf073226440ab4f10c01bce9
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-24 23:12:02 +0600

    practice Javascript Function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,19 @@
-// var and let and const
-// let and cosnt (ES6)
+//array iteration using forEach Function:
+var foods = ['cake', 'make','dake','take']
 
-// Global scope
-var a = 3;
-let b = 2; 
-const c = 4;
-const d = 90;
-
-console.log("Global Scope: ", a,b,c);
-
-function localScope(){
-    var a = 6;
-    let b = 7;
-    const c = 9;
-    console.log("Local Scope: ", a,b,c, d)
+let a_b = (item,index,whole_array)=>{
+    console.log(`index: ${index} item:${item} the wholeArray:${whole_array}`)
+    
 }
 
-localScope();
-console.log("Global Scope: ", a,b,c);
-
-if(true){
-    var a = 11;
-    let b = 12;
-    const c = 13;
-    console.log("If scope: ",a,b,c)
+let saysomthing = function(item,index){
+   
+    return `index:${index} item:${item}`
+    
 }
+let a;
+console.log(a)
+foods.forEach(a_b);
+let new_arr = foods.map(saysomthing)
+console.log(new_arr)
 
-console.log("Global Scope: ", a,b,c);
-
-
-for(var a = 0; a<10; a++){
-    console.log("for scope: ", a)
-}
-
-console.log("Global Scope: ", a,b,c);

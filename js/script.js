6bb4f4ed416095306ff1a0bc074f10988dd1452d
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-25 11:18:56 +0600

    practice OOP
    practice javascript oop

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-//array iteration using forEach Function:
-var foods = ['cake', 'make','dake','take']
+//class (ES6)
 
-let a_b = (item,index,whole_array)=>{
-    console.log(`index: ${index} item:${item} the wholeArray:${whole_array}`)
-    
+class Person {
+    constructor(fName,mName,lName,dob){
+        this.firstName = fName;
+        this.MiddleName = mName;
+        this.lastName = lName;
+        this.dob = dob;
+    }
+
+    calculateAge(){
+        let birthDay = new Date(this.dob);
+        let diff = Date.now() - birthDay.getTime();
+        let ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    FullName(){
+        console.log(`Your Full Name: ${this.firstName} ${this.MiddleName}  ${this.lastName}`)
+    }
 }
+let a = prompt('Enter your firstName: ')
+let b = prompt("Enter your Middle Name: ")
+let c = prompt("Enter Your Last Name: ")
+let d = prompt("Enter Your Date of birthDay: ")
 
-let saysomthing = function(item,index){
-   
-    return `index:${index} item:${item}`
-    
-}
-let a;
-console.log(a)
-foods.forEach(a_b);
-let new_arr = foods.map(saysomthing)
-console.log(new_arr)
-
+let person1 = new Person(a,b,c,d);
+console.log(person1)
+person1.FullName();
+let age = person1.calculateAge();
+console.log(age);

ba24cfd35e55424d11e7f7be79c1b3a0cd877547
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-26 11:33:17 +0600

    Dom practice

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ val = document.getElementById('document-id');
 val.style.color='white'
 val.style.padding = '10px'
 val.style.paddingLeft = '50px'
-console.log(val)
+//console.log(val)
 
 //changing content: 
 val.innerText="this is heading"
@@ -22,4 +22,39 @@ val = document.querySelector('#document-id')
 val = document.querySelector('simple-class')
 val = document.querySelector('li:nth-child(1')
 
-console.log(val)
+//console.log(val)
+
+
+val = document.getElementsByTagName('li');
+list = document.querySelector('ol').getElementsByTagName("li")
+console.log(list)
+
+lis = Array.from(list);
+lis.forEach(function(item){
+    console.log(item)
+})
+
+list = document.querySelector('#document-id')
+console.log(list)
+
+
+list = document.querySelectorAll('#document-id')
+
+list.forEach((item)=>{
+    console.log(item)
+})
+
+list = document.querySelectorAll('li:nth-child(odd)')
+console.log(list)
+list.forEach(function(item){
+    item.style.background='red'
+    item.style.color = 'gray'
+}) 
+
+let lieven = document.querySelectorAll('li:nth-child(even)');
+console.log(lieven)
+
+lieven.forEach(function(item){
+    item.style.background='gray';
+    item.style.color = 'red'
+})

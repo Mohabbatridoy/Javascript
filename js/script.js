7573a80aa82b419ba8f3f552ff45e1a341dccf2f
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-01-27 08:42:54 +0600

    DOM practice

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-//Traversing the dom
-let val ;
-let list = document.querySelector('ul')
-val = list;
+//dom
+//getElementsByClassName;
 
-console.log(val)
+let list = document.getElementsByClassName('sample-class');
+console.log(list[0])
 
-let listItem = document.querySelectorAll('ul li')
-console.log(listItem)
+list[0].style.background = 'red'
+
+///getElementsByTagName:
+list = document.getElementsByTagName('li')
+list = document.querySelector('ol').getElementsByTagName('li')
+// let liArray = Array.from(list)
+// liArray.forEach((item)=>{
+//     item.style.background = 'gray'
+// })
+// console.log(list)
+
+///query selector():
+
+list = document.querySelectorAll('ol li')
+console.log(list)
+
+list.forEach((item)=>{
+    item.style.color = 'red';
+})
+
+list = document.querySelectorAll('li:nth-child(odd)')
+list.forEach((item)=>{
+    item.style.background = 'gray';
+    item.style.color = 'red'
+})
+
+list = document.querySelectorAll('li:nth-child(even)')
+list.forEach(function(item){
+    item.style.background = 'red';
+    item.style.color = 'gray';
+    // item.innerText = 'this is example'
+})
